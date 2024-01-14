98be88d17bbdcc12a06cedf947da6462fcb39e61
KittydaCat <mccahey.fuerst@gmail.com>
2024-01-14 00:55:38 -0700

    Statmentizer done

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ use crate::bfasm::{Type};
 //     // value: Type,
 // );
 
+// Todo:
+// add += -= as funcs because they are sufficiently different
+
 
 // the last type is the return value
 // #[derive(Debug)]
@@ -27,11 +30,46 @@ enum Value {
 
 #[derive(Debug)]
 enum Statement{
+    If(Value, Vec<Statement>),
     Match(Value, Vec<(Type, Vec<Statement>)>),
     While(Value, Vec<Statement>),
     Function(String, Vec<Value>),
-    Assignment(String, Value)
+    // Assignment(String, Value) // replaced to make arrays it more consistent
     // Return(Value),
+}
+
+impl Statement {
+
+    fn print(code: &[Statement]){
+
+        for statement in code {
+
+            match statement {
+                Statement::If(ref cond, ref sub_code) => {
+                    println!("If {:?}:", cond);
+                    Statement::print(sub_code);
+                }
+                Statement::Match(ref cond, ref match_arms) => {
+                    println!("Match {:?}:", cond);
+
+                    for (cond, sub_code) in match_arms{
+                        println!("{:?} =>", cond);
+                        Statement::print(sub_code)
+                    }
+
+                }
+                Statement::While(ref cond, ref sub_code) => {
+                    println!("While {:?}:", cond);
+                    Statement::print(sub_code);
+                }
+                Statement::Function(_, _) => {
+                    println!("{:?}", statement);
+                }
+            }
+        }
+
+    }
+
 }
 
 #[derive(Debug, PartialEq)]
@@ -46,7 +84,7 @@ enum Token {
     CloseBrace,
     While,
     If,
-    Else,
+    // Else,
     Match,
     // Arrow,
     GreaterThan,
@@ -78,7 +116,7 @@ impl Token {
             Token::CloseBrace => {"}"}
             Token::While => {"while"}
             Token::If => {"if"}
-            Token::Else => {"else"}
+            // Token::Else => {"else"}
             Token::Match => {"match"}
             Token::GreaterThan => {">"}
             Token::LessThan => {"<"}
@@ -128,7 +166,7 @@ fn tokenize(code: &str) -> Option<Vec<Token>> {
                     "let" => Token::Let,
                     "while" => Token::While,
                     "if" => Token::If,
-                    "else" => Token::Else,
+                    // "else" => Token::Else,
                     "match" => Token::Match,
                     "mut" => Token::Mut,
                     str => Token::Name(String::from(str)),
@@ -212,35 +250,43 @@ fn tokens_to_statements(tokens: &[Token]) -> Result<Vec<Statement>, Option<usize
         match current_token {
             Token::Let => {
 
+                // todo combine
+
                 if let [T::Let, T::Name(ref var), T::Equal] = dbg!(&tokens[index..index+3]) {
                     index += 3;
                     let starting_index = index;
 
                     while tokens[index] != Token::SemiColon{
-                        index+=1
+                        index+=1;
                     };
 
                     statements.push(
-                        Statement::Assignment(
-                            var.clone(),
-                            tokens_to_value(dbg!(&tokens[starting_index..index])).unwrap()
+                        Statement::Function(
+                            String::from("="),
+                            vec![
+                                Value::Var(var.clone()),
+                                tokens_to_value(dbg!(&tokens[starting_index..index])).unwrap()
+                            ]
                         ));
 
                     index += 1;
+
                 } else if let [T::Let, T::Mut, T::Name(ref var), T::Equal] = dbg!(&tokens[index..index+4]){
                     index += 4;
                     let starting_index = index;
 
                     while tokens[index] != Token::SemiColon{
-                        index+=1
+                        index+=1;
                     };
 
-                    dbg!(starting_index, index, &tokens);
-
                     statements.push(
-                        Statement::Assignment(
-                            var.clone(),
-                            tokens_to_value(dbg!(&tokens[starting_index..index])).unwrap()));
+                        Statement::Function(
+                            String::from("="),
+                            vec![
+                                Value::Var(var.clone()),
+                                tokens_to_value(dbg!(&tokens[starting_index..index])).unwrap()
+                            ]
+                        ));
 
                     index += 1;
                 }
@@ -251,7 +297,6 @@ fn tokens_to_statements(tokens: &[Token]) -> Result<Vec<Statement>, Option<usize
                 let starting_index = index + 1; // move past the while token
 
                 dbg!(index);
-                dbg!(&tokens);
 
                 while tokens[index] != Token::OpenBrace{
                     index+=1
@@ -259,7 +304,7 @@ fn tokens_to_statements(tokens: &[Token]) -> Result<Vec<Statement>, Option<usize
 
                 let block_index = index;
 
-                index = find_next_balanced(&Token::OpenBrace, &tokens, index);
+                index = find_next_balanced(&tokens, index);
 
                 statements.push(Statement::While(
                     tokens_to_value(dbg!(&tokens[starting_index..block_index])).unwrap(),
@@ -267,19 +312,198 @@ fn tokens_to_statements(tokens: &[Token]) -> Result<Vec<Statement>, Option<usize
                 ));
 
                 index += 1;
+
+                // todo allow a semicolon after if while and match statements
             }
 
-            token => {panic!("{:?}", token)}
+            Token::Name(ref var) => {
+                index += 1;
+
+                if let [oper @ (T::Plus | T::Minus), T::Equal, T::Name(ref val), T::SemiColon]
+                    = &tokens[index..index + 4] {
+                    statements.push(
+                        Statement::Function(
+                            String::from("="),
+                            vec![
+                                Value::Var(var.clone()),
+                                Value::Func(oper.into(),
+                                            vec![
+                                                Value::Var(var.clone()),
+                                                Value::StaticValue(str_to_type(val).unwrap())
+                                            ])
+                            ]
+                        )
+                    );
+
+                    index += 4;
+                } else if let [T::OpenBracket, T::Name(ref var_index), T::CloseBracket, T::Equal]
+                    = &tokens[index..index + 4] {
+                    index += 4;
+                    let starting_index = index;
+
+                    while Token::SemiColon != tokens[index] { index += 1; }
+
+                    statements.push(
+                        Statement::Function(
+                            String::from("[]="),
+                            vec![
+                                Value::Var(var.clone()),
+                                str_to_value(var_index),
+                                tokens_to_value(&tokens[starting_index..index]).unwrap()
+                            ]
+                        )
+                    );
+
+                    index += 1;
+                }else if let Some([T::OpenBracket, T::Name(ref var_index), T::CloseBracket, oper @ (T::Plus | Token::Minus), T::Equal])
+                    = tokens.get(index..index + 5){
+
+                        index += 5;
+                        let starting_index = index;
+
+                        while Token::SemiColon != tokens[index] { index += 1; }
+
+                        statements.push(
+                            Statement::Function(
+                                String::from("[]="),// todo? if oper == T::Plus{"[]+="} else {"[]-="}),
+                                vec![
+                                    Value::Var(var.clone()),
+                                    str_to_value(var_index),
+                                    Value::Func(
+                                        String::from(oper),
+                                        vec![
+                                            tokens_to_value(&tokens[starting_index..index]).unwrap(),
+                                            Value::Func(
+                                                String::from("[]"),
+                                                vec![Value::Var(var.clone()), str_to_value(var_index)])
+                                        ])
+                                ]
+                            )
+                        );
+
+                        index += 1;
+
+                }else if let T::Dot | T::OpenParens = &tokens[index] {
+
+                    let starting_index = index;
+
+                    while tokens[index] != Token::SemiColon {index += 1;}
+
+                    // let Value::Func(func, args) = dbg!(tokens_to_value(dbg!(&tokens[starting_index-1..index])).unwrap()) else {
+                    //     todo!()
+                    // };
+                    //
+                    // statements.push(Statement::Function(func, args));
+
+                    let token = dbg!(&tokens[starting_index-1..index]);
+
+                    match dbg!(tokens_to_value(token)) {
+                        Some(Value::Func(func, args)) => {
+                            statements.push(Statement::Function(func, args))
+                        }
+
+                        val => panic!("{:?}", val)
+                    }
+
+                    // statements.push(Statement::Function(
+                    //     func_name.clone(),
+                    //     if dbg!(index) != dbg!(starting_index) {
+                    //         vec![
+                    //             Value::Var(var.clone()),
+                    //             tokens_to_value(&tokens[index..starting_index]).unwrap()
+                    //         ]
+                    //     } else {
+                    //         vec![Value::Var(var.clone())]
+                    //     }
+                    // ));
+
+                    index += 1;
+                } else if T::OpenParens == tokens[index]{
+
+
+
+                } else {panic!()}
+            }
+
+            Token::Match => {
+
+                let val_index = index + 1;
+
+                while tokens[index] != Token::OpenBrace{index+=1;}
+
+                let val = tokens_to_value(&tokens[val_index..index]).unwrap();
+
+                let mut clauses = Vec::new();
+
+                index += 1;
+
+                while let Some([T::Name(ref clause_val), T::Equal, T::GreaterThan, T::OpenBrace]) = dbg!(tokens.get(index..index+4)) {
+
+
+                    index += 3;
+
+                    let clause_index = index;
+
+                    index = find_next_balanced(&tokens, index);
+
+                    dbg!(index, clause_index);
+
+                    if clause_val != "_"{
+                        clauses.push((
+                            str_to_type(clause_val).unwrap(),
+                            tokens_to_statements(dbg!(&tokens[clause_index+1..index])).unwrap()
+                        ));
+                    } else {
+                        assert_eq!(&tokens[clause_index + 1..index], []); // todo
+                    }
+
+                    index += 1;
+
+                    if tokens[index] == Token::Comma {index+=1;}
+                }
+
+                statements.push(Statement::Match(val, clauses));
+
+                index += 1;
+
+                dbg!(index, &tokens);
+            },
+
+            Token::If => {
+                index += 1;
+
+                let val_index = index;
+
+                while *dbg!(&tokens[index]) != Token::OpenBrace{index += 1;}
+
+                let code_index = index;
+
+                index = find_next_balanced(&tokens, index);
+
+                statements.push(Statement::If(
+                    tokens_to_value(&tokens[val_index..code_index]).unwrap(),
+                    tokens_to_statements(&tokens[code_index+1..index]).unwrap(),
+                ));
+
+                index += 1;
+
+            },
+
+            token => {
+                panic!("{:?} {:?}", token, index)
+            }
         };
 
     }
 }
 
-// set index ahead of 1st instance of the value
+// set index at 1st instance of the value
 // the result will be the inverse of the token
-fn find_next_balanced(target: &Token, tokens: &[Token], mut index: usize) -> usize {
+fn find_next_balanced(tokens: &[Token], mut index: usize) -> usize {
 
-    let inv_target = match target {
+    let target = &tokens[index];
+
+    let inv_target = match tokens[index] {
 
         Token::OpenBrace => {Token::CloseBrace}
         Token::OpenParens => {Token::CloseParens}
@@ -289,15 +513,19 @@ fn find_next_balanced(target: &Token, tokens: &[Token], mut index: usize) -> usi
 
     let mut depth = 1;
 
-    while depth > 0{
+    index += 1;
 
-        index += 1; // move after if statements?
+    loop {
 
         if tokens[index] == *target{
             depth += 1;
         } else if tokens[index] == inv_target{
             depth -= 1;
         }
+
+        if depth == 0 {break};
+
+        index += 1;
     }
 
     index
@@ -305,7 +533,7 @@ fn find_next_balanced(target: &Token, tokens: &[Token], mut index: usize) -> usi
 
 fn tokens_to_value(tokens: &[Token]) -> Option<Value> {
 
-    let mut index = 1;
+    let mut index = 0;
 
     let Token::Name(ref str) = dbg!(&tokens[0]) else {
         dbg!(&tokens[0]);
@@ -332,7 +560,6 @@ fn tokens_to_value(tokens: &[Token]) -> Option<Value> {
                 if index != 2 {
                     Vec::from([tokens_to_value(&tokens[2..index]).unwrap()])
                 } else {
-                    index += 1;
                     Vec::new()
                 }
             )
@@ -350,7 +577,6 @@ fn tokens_to_value(tokens: &[Token]) -> Option<Value> {
                     if index != 4{
                         Vec::from([Value::Var(str.clone()), tokens_to_value(dbg!(&tokens[4..index])).unwrap()])
                     } else {
-                        index += 1;
                         Vec::from([Value::Var(str.clone())])
                     }
                 )
@@ -362,6 +588,8 @@ fn tokens_to_value(tokens: &[Token]) -> Option<Value> {
     };
 
     dbg!(&val);
+
+    index += 1; // either then index is still on the str or its on the last bracket or brace from
 
 
     let Some(operand) = dbg!(tokens.get(index)) else {
@@ -418,7 +646,7 @@ mod tests {
     use super::*;
 
     # [test]
-    fn program(){
+    fn program() {
         let file = fs::read_to_string("./src/program.rs").unwrap();
 
         let file = &file[file.find("fn main()").unwrap()..];
@@ -427,27 +655,72 @@ mod tests {
 
         let statements = tokens_to_statements(&tokens[5..tokens.len()-1]).unwrap();
 
-        println!("{:?}\n{:?}", tokens, statements)
+        // println!("{:?}\n{:?}", tokens, statements)
+
+        Statement::print(&statements)
     }
 
-    # [test]
-    fn program_test(){
-
-        use crate::program::*;
-
-        main()
-
-    }
+    // # [test]
+    // fn program_test(){
+    //
+    //     use crate::program::*;
+    //
+    //     main()
+    //
+    // }
 
     #[test]
-    fn while_test(){
-        let code = "while program_index < program.len() {}";
+    fn match_test() {
+        let code = "match program[program_index]{\
+        'a' => {},\
+        'b' => {index += 1;}\
+        'c' => {\
+            if index == 3 {
+                index += 1;
+            }
+        }
+        _ => {}
+        }";
 
         println!("{:?}", tokens_to_statements(&tokenize(code).unwrap()).unwrap());
     }
 
     #[test]
-    fn program_lets(){
+    fn if_test() {
+        let code = "if program_index < program.len() {
+            let x = 5;
+            if x {
+                x -= 1;
+            }
+        }";
+
+        println!("{:?}", tokens_to_statements(&tokenize(code).unwrap()).unwrap());
+    }
+
+    #[test]
+    fn var_line() {
+        let code = "let array = new_array();\
+        array[0] = input();\
+        array[x] = array[0];\
+        array.push(0);";
+
+        println!("{:?}", tokens_to_statements(&tokenize(code).unwrap()).unwrap());
+    }
+
+    #[test]
+    fn while_test() {
+        let code = "while program_index < program.len() {
+            let x = 5;
+            while x {
+                x -= 1;
+            }
+        }";
+
+        println!("{:?}", tokens_to_statements(&tokenize(code).unwrap()).unwrap());
+    }
+
+    #[test]
+    fn program_lets() {
         let code = "let program = input_str();
         let mut program_index = 0;
         let mut array = new_array();
@@ -458,7 +731,7 @@ mod tests {
     }
 
     #[test]
-    fn let_w_func(){
+    fn let_w_func() {
         let code = "let a = f();\
         let b = array[array_index];\
         let mut c = a;";
@@ -469,7 +742,7 @@ mod tests {
     }
 
     # [test]
-    fn let_to_statements(){
+    fn let_to_statements() {
         let code = "let a = 100;\
         let b = 90;\
         let mut c = a;";
@@ -478,7 +751,7 @@ mod tests {
     }
 
     # [test]
-    fn let_to_ast(){
+    fn let_to_ast() {
         let code = "let a = 100;\
         let b = 90;\
         let c = a;";
